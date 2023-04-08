a8b83e8e3af1a78b138a4e73c2d50aad75a48c7e
Ethan Stanks <EJStanks@student.fullsail.edu>
2023-04-08 16:01:34 -0400

    Added Log In with Google and changed the manifest to have SignInActivity as the launching activity.

--- app/src/main/java/com/example/cranny/SignInActivity.kt
+++ app/src/main/java/com/example/cranny/SignInActivity.kt
@@ -1,11 +1,61 @@
 package com.example.cranny
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var googleSignInButton: Button
+    private lateinit var client: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        googleSignInButton = findViewById(R.id.bGoogleSignIn)
+
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        client = GoogleSignIn.getClient(this, options)
+        googleSignInButton.setOnClickListener{
+            val intent = client.signInIntent
+            startActivityForResult(intent, 10001)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==10001){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account = task.getResult(ApiException::class.java)
+            val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+            FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener{task->
+                    if(task.isSuccessful){
+
+                        val i  = Intent(this,MainActivity::class.java)
+                        startActivity(i)
+
+                    }else{
+                        Toast.makeText(this,task.exception?.message,Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        if(FirebaseAuth.getInstance().currentUser != null){
+            val i  = Intent(this,MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
