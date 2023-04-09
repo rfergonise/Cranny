0ce156b8503b74a3d9f9aeffbd9538d2f41dde97
Will Hoover <billhoover070@gmail.com>
2023-04-08 22:15:50 -0400

    Created book data class on BookDataClass branch

---
+++ app/src/main/java/com/example/cranny/Book.kt
@@ -0,0 +1,7 @@
+package com.example.cranny
+
+data class Book(
+    var title: String = "not entered", var authorName: String = "not entered", var publicationYear: Long = 1776, var starRating: Int = 0, var publisher: String = "not entered"
+
+
+)
