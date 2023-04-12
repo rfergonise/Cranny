c8a8b1c23e4f509bc2ef90cdb9e65b44bbefc6d5
Will Hoover <billhoover070@gmail.com>
2023-04-12 12:32:38 -0400

    updated book class with variables written on paper prototype

--- app/src/main/java/com/example/cranny/Book.kt
+++ app/src/main/java/com/example/cranny/Book.kt
@@ -1,7 +1,17 @@
 package com.example.cranny
 
 data class Book(
-    var title: String = "not entered", var authorName: String = "not entered", var publicationYear: Long = 1776, var starRating: Int = 0, var publisher: String = "not entered"
+    var id: String,
+    var title: String,
+    var authorNames: List<String>,
+    var publicationDate: String?,
+    var starRating: Int?,
+    var publisher: String?,
+    var description: String?,
+    var pageCount: Int?,
+    var thumbnail: String?,
+    var journalEntry: String?,
+    var userProgress: Int?,
 
 
 )
