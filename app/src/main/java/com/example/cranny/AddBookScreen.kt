af50bf7f9af3b770d8c6b3153f6e60255bf18b20
krowley011 <KRStevens1@student.fullsail.edu>
2023-04-13 09:54:52 -0500

    Started add book screen. Added some text boxes for user input. Need to figure out autofill for date if no date is entered.

---
+++ app/src/main/java/com/example/cranny/AddBookScreen.kt
@@ -0,0 +1,12 @@
+package com.example.cranny
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBookScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_book_screen)
+
+    }
+}
