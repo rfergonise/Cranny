52440bd5f677b31ed527cb230c601481259f0af4
krowley011 <KRStevens1@student.fullsail.edu>
2023-04-14 10:59:29 -0500

    Updated more of the add book page. Didn't previously have it in scroll view, but it now scrolls as intended.

--- app/src/main/java/com/example/cranny/AddBookPage.kt
+++ app/src/main/java/com/example/cranny/AddBookPage.kt
@@ -3,10 +3,10 @@ package com.example.cranny
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AddBookScreen : AppCompatActivity() {
+class AddBookPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_book_screen)
+        setContentView(R.layout.activity_add_book_page)
 
     }
 }
