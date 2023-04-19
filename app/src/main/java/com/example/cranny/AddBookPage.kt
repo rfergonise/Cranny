2ddf5eb00637710b17d095bb024b200e8f05eb17
krowley011 <KRStevens1@student.fullsail.edu>
2023-04-18 21:03:10 -0500

    Updated layout of text boxes, added review bar, started trying to implement code to save user input to a variable to display elsewhere.

--- app/src/main/java/com/example/cranny/AddBookPage.kt
+++ app/src/main/java/com/example/cranny/AddBookPage.kt
@@ -2,11 +2,29 @@ package com.example.cranny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.*
 
 class AddBookPage : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_book_page)
+
+        val TitleInput=findViewById<EditText>(R.id.etTitleInput)
+        val AuthorInput=findViewById<EditText>(R.id.etAuthorInput)
+        val SummaryInput=findViewById<EditText>(R.id.etSummaryInput)
+        val PurchasedFromInput=findViewById<EditText>(R.id.etPurchasedFrom)
+        val ReviewInput=findViewById<EditText>(R.id.etReview)
+        val LastPageReadInput=findViewById<TextView>(R.id.tvPageRead)
+        val DataStartedInput=findViewById<TextView>(R.id.tvDateStarted)
+        val FinishedCB=findViewById<CheckBox>(R.id.cbFinished)
+        val CancelBTN=findViewById<Button>(R.id.btnCancel)
+        val SaveBTN=findViewById<Button>(R.id.btnSave)
 
     }
 }
