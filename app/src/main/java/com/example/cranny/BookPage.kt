70cb033d50a7e9b05971c6e79fd78428e8dc78ea
robert fergonise <RMFergonise@student.fullsail.edu>
2023-04-18 20:40:43 -0700

    robert commit..

---
+++ app/src/main/java/com/example/cranny/BookPage.kt
@@ -0,0 +1,11 @@
+package com.example.cranny
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_page)
+    }
+}
