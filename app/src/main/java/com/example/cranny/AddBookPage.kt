83eb71cf0859b24e50eaef248e5a0cc669828dd4
krowley011 <KRStevens1@student.fullsail.edu>
2023-04-23 21:26:55 -0500

    Added all of the variables that should be needed for the add book screen. Set a check listener for the finished checkbox to display either the data finished text view or the last page read text view.

--- app/src/main/java/com/example/cranny/AddBookPage.kt
+++ app/src/main/java/com/example/cranny/AddBookPage.kt
@@ -2,12 +2,12 @@ package com.example.cranny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.RatingBar
 import android.widget.TextView
-import android.widget.Toast
-import android.widget.Toast.*
 
 class AddBookPage : AppCompatActivity() {
 
@@ -15,16 +15,37 @@ class AddBookPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_book_page)
 
-        val TitleInput=findViewById<EditText>(R.id.etTitleInput)
-        val AuthorInput=findViewById<EditText>(R.id.etAuthorInput)
-        val SummaryInput=findViewById<EditText>(R.id.etSummaryInput)
-        val PurchasedFromInput=findViewById<EditText>(R.id.etPurchasedFrom)
-        val ReviewInput=findViewById<EditText>(R.id.etReview)
-        val LastPageReadInput=findViewById<TextView>(R.id.tvPageRead)
-        val DataStartedInput=findViewById<TextView>(R.id.tvDateStarted)
-        val FinishedCB=findViewById<CheckBox>(R.id.cbFinished)
-        val CancelBTN=findViewById<Button>(R.id.btnCancel)
-        val SaveBTN=findViewById<Button>(R.id.btnSave)
+        val titleInput = findViewById<EditText>(R.id.etTitleInput)
+        val dataStartedInput = findViewById<TextView>(R.id.tvDateStarted)
+        val authorInput = findViewById<EditText>(R.id.etAuthorInput)
+        val summaryInput = findViewById<EditText>(R.id.etSummaryInput)
+        val purchasedFromInput = findViewById<EditText>(R.id.etPurchasedFrom)
+        val reviewInput = findViewById<EditText>(R.id.etReview)
+        val genresInput = findViewById<EditText>(R.id.tiGenres)
+        val tagsInput = findViewById<EditText>(R.id.tiTags)
+        val ratingsInput = findViewById<RatingBar>(R.id.ratingBar)
+        val cancelBTN = findViewById<Button>(R.id.btnCancel)
+        val saveBTN = findViewById<Button>(R.id.btnSave)
+        val finishedCB = findViewById<CheckBox>(R.id.cbFinished)
+        val dateFinishedTextView = findViewById<TextView>(R.id.tvDateFinished)
+        val dateFinishedInput = findViewById<EditText>(R.id.etDateFinished)
+        val lastPageReadTextView = findViewById<TextView>(R.id.tvPageRead)
+        val lastPageReadInput = findViewById<EditText>(R.id.tnPageNumber)
+
+        // Hide Data Finished or Last Page Read based on finished checkbox
+        finishedCB.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (finishedCB.isChecked) {
+                dateFinishedTextView.visibility = View.VISIBLE
+                dateFinishedInput.visibility = View.VISIBLE
+                lastPageReadTextView.visibility = View.INVISIBLE
+                lastPageReadInput.visibility = View.INVISIBLE
+            } else {
+                dateFinishedTextView.visibility = View.INVISIBLE
+                dateFinishedInput.visibility = View.INVISIBLE
+                lastPageReadTextView.visibility = View.VISIBLE
+                lastPageReadInput.visibility = View.VISIBLE
+            }
+        }
 
     }
 }
